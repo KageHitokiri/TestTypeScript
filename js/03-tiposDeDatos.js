9549655593ce7b8627a80f6adf7f3d7582bc4631
Carlos <skhcri@gmail.com>
2021-03-29 05:38:27 +0200

    Tipos de datos

---
+++ js/03-tiposDeDatos.js
@@ -0,0 +1,29 @@
+/**
+ * String
+ */
+var message;
+message = "Esto es una cadena de texto";
+/**
+ * Number
+ */
+var age;
+age = 15;
+/**
+ * Boolean
+ */
+var trueOrFalse;
+trueOrFalse = false;
+/**
+ * Any
+ * Cualquier tipo de dato
+ */
+var anyTypeofData;
+anyTypeofData = false;
+/**
+ * Arrays
+ */
+var languajes;
+languajes = ["PHP", "TypeScript", "Java"];
+var numbers;
+numbers = [12, 5, 20, 7];
+console.log(message, age, trueOrFalse, anyTypeofData, languajes, numbers);
---
+++ js/03-tiposDeDatos.ts
@@ -0,0 +1,39 @@
+/**
+ * String
+ */
+var message;
+
+message="Esto es una cadena de texto";
+
+/**
+ * Number
+ */
+
+var age: number;
+age=15;
+
+/**
+ * Boolean
+ */
+
+var trueOrFalse:boolean;
+trueOrFalse =false;
+
+/**
+ * Any
+ * Cualquier tipo de dato
+ */
+
+var anyTypeofData:any;
+anyTypeofData = false;
+
+/**
+ * Arrays
+ */
+var languajes: Array<string>;
+languajes=["PHP","TypeScript","Java"];
+
+var numbers: number[];
+numbers = [12,5,20,7];
+
+console.log(message, age, trueOrFalse, anyTypeofData, languajes, numbers);
