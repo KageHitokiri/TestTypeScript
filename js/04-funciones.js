a630cfdfc8ddacade405f6eae68c2dbfe39f7def
Carlos <skhcri@gmail.com>
2021-03-29 05:51:16 +0200

    funciones básicas con ts

---
+++ js/04-funciones.js
@@ -0,0 +1,5 @@
+function getNumber(number1) {
+    if (number1 === void 0) { number1 = 7; }
+    return "El numero es " + number1;
+}
+console.log(getNumber());
---
+++ js/04-funciones.ts
@@ -0,0 +1,5 @@
+function getNumber(number1:number = 7):string{
+    return "El numero es "+number1;
+}
+
+console.log(getNumber());
